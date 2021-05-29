fb5786ab3b9126b937315bde92236a87b797b015
Pavel varecz <pavel.varecz@gmail.com>
2021-05-30 00:38:30 +0300

    added validate fields

--- src/Services/AccountServices/Registration/RegistrationNaturalPerson.vue
+++ src/Services/AccountServices/Registration/RegistrationNaturalPerson.vue
@@ -6,7 +6,9 @@
           <label>Контактное лицо</label>
         </div>
         <div class="col">
-          <input type="text" class="form-control" required v-model="model.name">
+          <input type="text" class="form-control" required v-model="state.model.name">
+          <span v-if="v$.model.name.$error">
+          {{ v$.model.name.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-2">
@@ -14,7 +16,9 @@
           <label>Наименование организации</label>
         </div>
         <div class="col">
-          <input type="text" class="form-control" required v-model="model.nameOrganization">
+          <input type="text" class="form-control" required v-model="state.model.nameOrganization">
+          <span v-if="v$.model.nameOrganization.$error">
+          {{ v$.model.nameOrganization.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-4">
@@ -22,7 +26,9 @@
           <label>Моб. телефон</label>
         </div>
         <div class="col">
-          <input type="text" class="form-control" required v-model="model.phoneNumber">
+          <input type="text" class="form-control" required v-model="state.model.phoneNumber">
+          <span v-if="v$.model.phoneNumber.$error">
+          {{ v$.model.phoneNumber.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-4">
@@ -30,7 +36,7 @@
           <label>Доп контакт</label>
         </div>
         <div class="col">
-          <input type="text" class="form-control" required v-model="model.anotherContact">
+          <input type="text" class="form-control" required v-model="state.model.anotherContact">
         </div>
       </div>
     </div>
@@ -45,10 +51,12 @@
         <div class="col">
           <div class="input-group">
             <div class="input-group-prepend">
-              <span class="input-group-text">@</span>
+              <label class="input-group-text">@</label>
             </div>
-            <input type="text" class="form-control" aria-describedby="validationTooltipUsernamePrepend" v-model="model.email">
+            <input type="text" class="form-control" aria-describedby="validationTooltipUsernamePrepend" v-model="state.model.email">
           </div>
+          <span v-if="v$.model.email.$error">
+          {{ v$.model.email.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-4 ">
@@ -56,7 +64,9 @@
           <label>Логин</label>
         </div>
         <div class="col">
-          <input type="text" class="form-control" aria-describedby="validationTooltipUsernamePrepend" v-model="model.userName">
+          <input type="text" class="form-control" aria-describedby="validationTooltipUsernamePrepend" v-model="state.model.userName">
+          <span v-if="v$.model.userName.$error">
+          {{ v$.model.userName.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-4 ">
@@ -64,7 +74,9 @@
           <label>Пароль</label>
         </div>
         <div class="col">
-          <input type="password" class="form-control" placeholder="Пароль" v-model="model.password">
+          <input type="password" class="form-control" placeholder="Пароль" v-model="state.model.password">
+          <span v-if="v$.model.password.$error">
+          {{ v$.model.password.$errors[0].$message }}</span>
         </div>
       </div>
       <div class="row mb-4 ">
@@ -72,7 +84,9 @@
           <label>Подтвердите пароль</label>
         </div>
         <div class="col">
-          <input type="password" class="form-control" placeholder="Подтвердите пароль" v-model="model.passwordConfirm">
+          <input type="password" class="form-control" placeholder="Подтвердите пароль" v-model="state.model.passwordConfirm">
+          <span v-if="v$.model.passwordConfirm.$error">
+          {{ v$.model.passwordConfirm.$errors[0].$message }}</span>
         </div>
       </div>
     </div>
@@ -95,6 +109,9 @@ import { useNotificationStore } from '@dafcoe/vue-notification'
 import Constants from "@/Services/Constants";
 import Model from "@/Models/UserModel"
 import AccountService from "@/Services/AccountServices/AccountService"
+import useValidate from '@vuelidate/core'
+import { required,email, minLength,sameAs, helpers,maxLength } from '@vuelidate/validators'
+import { reactive, computed } from 'vue'
 export default {
 name: "RegistrationProvider",
   components:{
@@ -102,10 +119,88 @@ name: "RegistrationProvider",
   },
   data() {
     return {
-      model:Model.data().registerModel,
       confirmEmail:"",
       isVisible:false
     }
+  },
+  setup() {
+    const state = reactive({
+      model:Model.data().registerModel
+    })
+    const rules = computed(() => {
+      return {
+        model: {
+          name:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ),
+            minLength: helpers.withMessage(
+                'Минимальная длинна 6 символов!',
+                minLength(6)
+            )
+          },
+          nameOrganization:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ),
+            minLength: helpers.withMessage(
+                'Минимальная длинна 6 символов!',
+                minLength(6)
+            )
+          },
+
+          phoneNumber:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ),
+            minLength: helpers.withMessage(
+                'Минимальная длинна 9 символов!',
+                minLength(9)
+            ),
+          },
+          userName:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ),
+            minLength: helpers.withMessage(
+                'Минимальная длинна 6 символов!',
+                minLength(6)
+            ),
+          },
+          email:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ), email:
+                helpers.withMessage(
+                    'Неверный формат почты!',
+                    email
+                ),
+          },
+          password:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ), minLength: helpers.withMessage(
+                'Минимальная длинна 6 символов!',
+                minLength(6)
+            )
+          },
+          passwordConfirm:{ required: helpers.withMessage(
+                'Поле является обязательным!',
+                required
+            ), sameAs: helpers.withMessage(
+                'Пароли не совпадают!',
+                sameAs(state.model.password)
+            )
+          },
+
+        },
+
+      }
+    })
+
+    const v$ = useValidate(rules, state)
+
+    return { state, v$ }
   },
   methods:{
     SendConfirmEmail(){
@@ -124,47 +219,58 @@ name: "RegistrationProvider",
       });
     },
     RegistrationUser(){
+      this.v$.$validate();
+      if (!this.v$.$error) { // if ANY fail validation
+        AccountService.methods.RegisterUser(this.state.model).then(response=>{
+          console.log(response);
+          const { setNotification } = useNotificationStore()
+          setNotification(Constants.methods.GetNotification(response.data.message,"info"));
+          this.isVisible = true;
 
-      AccountService.methods.RegisterUser(this.model).then(response=>{
-        console.log(response);
-        const { setNotification } = useNotificationStore()
-        setNotification(Constants.methods.GetNotification(response.data.message,"info"));
-        this.isVisible = true;
 
+        }).catch(error=>{
+          if(error.response.data.Email){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.Email[0],"alert"));
+          }
+          if(error.response.data.PhoneNumber){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.PhoneNumber[0],"alert"));
+          }
+          if(error.response.data.PasswordConfirm){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.PasswordConfirm[0],"alert"));
+          }
+          if(error.response.data.NameOrganization){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.NameOrganization[0],"alert"));
+          }
+          if(error.response.data.Name){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.Name[0],"alert"));
+          }
+          if(error.response.data.message){
+            const { setNotification } = useNotificationStore()
+            setNotification(Constants.methods.GetNotification(error.response.data.message,"alert"));
+          }
 
-      }).catch(error=>{
-        if(error.response.data.Email){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.Email[0],"alert"));
-        }
-        if(error.response.data.PhoneNumber){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.PhoneNumber[0],"alert"));
-        }
-        if(error.response.data.PasswordConfirm){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.PasswordConfirm[0],"alert"));
-        }
-        if(error.response.data.NameOrganization){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.NameOrganization[0],"alert"));
-        }
-        if(error.response.data.Name){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.Name[0],"alert"));
-        }
-        if(error.response.data.message){
-          const { setNotification } = useNotificationStore()
-          setNotification(Constants.methods.GetNotification(error.response.data.message,"alert"));
-        }
-
-        console.log(error);
-      })
+          console.log(error);
+        })
+      } else {
+        alert('Пожалуйста, введите данные корректно!');
+      }
+      /**/
     }
   }
 }
 </script>
 
 <style scoped>
-
+span{
+  margin: 0;
+  padding: 0;
+  font-size: x-small;
+  color: red;
+  position: relative;
+}
 </style>
